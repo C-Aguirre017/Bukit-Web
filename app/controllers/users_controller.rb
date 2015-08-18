eb07661b486125dc25851385c0e35216cd4ae28c
C-Aguirre017 <c.aguirre017@gmail.com>
2015-08-18 19:17:05 -0300

    Update #Create User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -101,6 +101,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :profession, :email, :phone)
+      params.require(:user).permit(:name, :profession, :email, :phone, :uid, :fbsecrettoken)
     end
 end
