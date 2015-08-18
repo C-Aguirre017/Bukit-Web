e0d7a2c2324a976a8f732ba6d71c0ff55033ba93
C-Aguirre017 <c.aguirre017@gmail.com>
2015-08-18 18:05:15 -0300

    Routes Añadidas

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   #Usuarios
   resources :users
   get 'fb/:uid' => 'users#show', as: :fb_uid
-  post "users/register" => "users#create", :as => :user_register
+  post 'users/register' => "users#create", :as => :user_register
 
   resources :courses, only: [:show,:index]
   resources :universities, only: [:show,:index]
